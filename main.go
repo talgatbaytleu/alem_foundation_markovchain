ba6e30b7a9e33610b917995d515a35b7d746f4b5
tbaitleu <jfiiloose@gmail.com>
2024-08-31 00:16:26 +0500

    testing how bufio.Scanner works

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
 	// cmd.Markov()
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+	fmt.Println(input)
+	scanner.Scan()
+	input = scanner.Text()
+	fmt.Println(input)
+	scanner.Scan()
+	input = scanner.Text()
+	fmt.Println(input)
+	scanner.Scan()
+	input = scanner.Text()
+	fmt.Println(input)
+	scanner.Scan()
+	input = scanner.Text()
+	fmt.Println(input)
+	scanner.Scan()
+	input = scanner.Text()
+	fmt.Println(input)
+	scanner.Scan()
+	input = scanner.Text()
+	fmt.Println(input)
 }
